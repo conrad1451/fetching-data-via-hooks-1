b63e70c5c1006c4c7726db0f9c1c931fb34e157d
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-07-10 21:13:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MyComponent from "./myComponents/new-mycomponent.js";
 export default function App() {
   return (
     <div className="App">
-      <MyComponent />
+      // <MyComponent />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
